7ce4a440d4bc3664c1c67b6f6d106b3bfa25dda8
Alex ER <100380496+kolik1983@users.noreply.github.com>
2022-03-23 23:09:51 +0300

    Add files via upload

---
+++ 12.7.3.py
@@ -0,0 +1,13 @@
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+
+money = int(input('Введите сумму вклада:'))
+
+tkb = int(per_cent.get('ТКБ') * money / 100)
+skb = int(per_cent.get('СКБ') * money / 100)
+vtb = int(per_cent.get('ВТБ') * money / 100)
+sber = int(per_cent.get('СБЕР') * money / 100)
+
+profit = [tkb, skb, vtb, sber]
+print("deposit=", profit)
+max_profit = max(profit)
+print("Максимальная сумма, которую вы можете заработать —", max_profit)
